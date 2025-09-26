32a7b57fe25ade80ea81ea693737bf94c71b8a7b
Firepeashooter1 <firepeashooter1@gmail.com>
2025-09-26 01:41:04 +0000

    Finished code

--- fileIO.cpp
+++ fileIO.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 
+int main() {
+    std::ifstream file("data.csv");
+    std::string line;
+
+    while (std::getline(file, line)) {
+        std::stringstream ss(line);
+
+        int numOne, numTwo;
+	char comma;
+
+        std::string word;
+
+        ss >> numOne >> comma >> numTwo >> comma >> word;
+
+        int total = numOne + numTwo;
+
+        for (int i = 0; i < total; ++i) {
+            std::cout << word << " ";
+        }//End for
+	std::cout << "\n";
+    }//End while
+
+}//End main
