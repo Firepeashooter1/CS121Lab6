b24b1120a8e6705edf2b5c0dc3a655c235088010
Firepeashooter1 <firepeashooter1@gmail.com>
2025-09-26 01:27:02 +0000

    resarted

--- fileIO.cpp
+++ fileIO.cpp
@@ -1,14 +1,1 @@
-#include <fstream>
-#include <iostream>
-#include <string>
 
-int main() {
-	std::ofstream outFile;
-	outFile.open("data.csv");
-	
-	if (outFile.is_open()){
-		outFile << "1, 2, " << std::endl;
-		outFile.close();
-	}
-
-}//End main
